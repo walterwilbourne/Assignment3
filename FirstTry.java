86284990cf72bff3011c8d3c2370c53d019f2977
walterwilbourne <79112520+walterwilbourne@users.noreply.github.com>
2021-02-15 15:25:46 -0500

    Add files via upload

---
+++ FirstTry.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+public class FirstTry {
+
+	public static void main(String[] args) {
+		
+		List<Object> array = new ArrayList<Object>();
+		String E= "e";
+		List <String> c = new ArrayList<String>(); 
+		
+		ClassName object = new ClassName();
+		array.add(E);
+		array.contains(object);
+		array.set(0,E);
+		array.retainAll(c);
+		array.remove(object);
+		
+		System.out.print(array);
+
+	}
+
+}
